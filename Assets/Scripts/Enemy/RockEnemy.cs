8ff9f45dd5ac89cf69e8614c459736bcfb56c111
Abel <abelpujol99@gmail.com>
2022-05-08 21:47:26 +0200

    Movement Done

--- Assets/Scripts/Enemy/RockEnemy.cs
+++ Assets/Scripts/Enemy/RockEnemy.cs
@@ -1,14 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Character;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class RockEnemy : MonoBehaviour
 {
-
-    [SerializeField] private Player _player;
     
+    [SerializeField] private Player _player;
+
+    [SerializeField]private Vector3 _targetPosition;
+
+    private float _distanceToMoveOnYAxis = 0.3f;
+
+    private void Start()
+    {
+        _targetPosition = new Vector3(transform.position.x, transform.position.y + _distanceToMoveOnYAxis,
+            transform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        ChangeTargetPosition();
+        
+        Move();
+    }
+
+    private void ChangeTargetPosition()
+    {
+        if (transform.position == _targetPosition)
+        {
+            _distanceToMoveOnYAxis *= -1;
+            _targetPosition = new Vector3(transform.position.x, transform.position.y + _distanceToMoveOnYAxis,
+                transform.position.z);
+        }
+    }
+
+    private void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, Math.Abs(_distanceToMoveOnYAxis) * Time.deltaTime) ;
+    }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
