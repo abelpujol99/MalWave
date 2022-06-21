bc84933ba0392e418a9d514172ba1356f46cc092
Abel <abelpujol99@gmail.com>
2022-06-21 13:35:41 +0200

    Pop-ups fixed and upgraded

--- Assets/Scripts/Event/ControlAdvise.cs
+++ Assets/Scripts/Event/ControlAdvise.cs
@@ -9,14 +9,20 @@ public class ControlAdvise : MonoBehaviour
 {
 
     [SerializeField] private UIMenu _mainMenu;
+
+    [SerializeField] private TextMeshProUGUI _text;
+
+    [SerializeField] private KeyCode _keyCode;
+    
     private void Start()
     {
         Time.timeScale = 0f;
+        _text.SetText("Press " + (char)34 + _keyCode + (char)34 + " to back to game");
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.F))
+        if (Input.GetKey(_keyCode))
         {
             _mainMenu.SetTutorialActive(false);
             Time.timeScale = 1f;
